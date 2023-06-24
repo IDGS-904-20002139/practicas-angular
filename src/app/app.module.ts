6dd4731b345a705ae6bdea157a1bccb087a72552
Paula Valencia <pau_doly@hotmail.com>
2023-06-23 23:01:59 -0600

    PDVP 23/06/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResistenciasComponent } from './resistencias/resistencias.component';
@@ -16,6 +16,7 @@ import { DistanciasModule } from './distancias-module/distancias.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     DistanciasModule
   ],
   providers: [],
--- src/app/resistencias/resistencias.component.ts
+++ src/app/resistencias/resistencias.component.ts
@@ -6,11 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./resistencias.component.css']
 })
 export class ResistenciasComponent {
-  banda1 = [
+  resultado!:number;
+  valorTolerancia:string = '';
+  banda1:string ='';
+  banda2:string ='';
+  multi:string ='';
+  operacionSeleccionada:string='black';
+  coloresBanda = [
     'black',
     'brown',
     'red',
-    'orange',
+    'oranje',
     'yellow',
     'green',
     'blue',
@@ -19,11 +25,55 @@ export class ResistenciasComponent {
     'white'
   ];
 
-  calcular(){
-  switch(this.banda1){
-    case 'black': ;
-        break;
-    }
+  opcionBanda = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+  ];
+
+  toleranciaColor = [
+    'gold',
+    'silver'
+  ];
+
+  valueb1:number=0;
+  valueb2:number=0;
+  valuemulti:number=0;
+  valorResistencia:number=0;
+  valorMax:number=0;
+  valorMin:number=0;
+  calcularResistencia(){
+    this.valueb1 = this.calcularbanda(this.banda1)
+    this.valueb2 = this.calcularbanda(this.banda2)
+    this.valuemulti = this.calcularbanda(this.multi)
+
+    this.valorResistencia = parseInt(this.valueb1.toString() + '' + this.valueb2.toString()) * 10**this.valuemulti
+    this.valorMax = this.valorResistencia * (1 + parseFloat(this.valorTolerancia))
+    this.valorMin = this.valorResistencia * (1 - parseFloat(this.valorTolerancia))
+    
   }
   
+  calcularbanda(banda:string){
+    switch(banda){
+      case 'black': return 0;
+          break;
+      case 'brown': return 1;
+          break;
+      case 'red': return 2;
+          break;
+      case 'oranje': return 3;
+          break;
+      case 'yellow': return 4;
+          break;
+      case 'green': return 5;
+          break;
+      case 'blue': return 6;
+          break;
+      case 'purple': return 7;
+          break;
+      case 'gray': return 8;
+          break;
+      case 'white': return 9;
+          break;
+          default: return 0;
+      }
+  }
 }
